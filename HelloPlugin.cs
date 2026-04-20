72a65808bb1ec0566b3a5cf68cbd7c5c4025af46
fiinix00 <fiinix00@gmail.com>
2026-04-20 13:08:42 +0200

    Add example plugin

---
+++ HelloPlugin.cs
@@ -0,0 +1,16 @@
+﻿using Krynetic.Database.PluginSystem;
+
+[module: PluginExport(typeof(HelloPlugin))]
+
+[Plugin(
+    "Hello",
+    Description = "Example plugin that says hello",
+    Tags = ["example"],
+    Stage = PluginLifecycle.Production,
+    Tier = LicenseTier.Community
+)]
+public static partial class HelloPlugin
+{
+    [PluginFunction("Hello")]
+    public static string Hello() => "Hello";
+}
